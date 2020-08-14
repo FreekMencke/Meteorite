451d6739093b713b12c84a04998e9a7c94465f9f
Freek Mencke <freek_mencke@outlook.com>
2020-08-14 19:46:02 +0200

    feat(home): switch /home path to /

--- projects/meteorite/src/app/app-routing.module.ts
+++ projects/meteorite/src/app/app-routing.module.ts
@@ -2,9 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'home' },
   {
-    path: 'home',
+    path: '',
     pathMatch: 'full',
     loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule),
   },
